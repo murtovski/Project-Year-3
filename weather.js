2a401b6cad199408a38e1fcd2b84c40e43558569
murtovski <murta1991@gmail.com>
2023-03-03 18:38:30 +0000

    added js weather functionality, need to continue changing the code to suit the page

--- weather.js
+++ weather.js
@@ -1,23 +1,53 @@
-window.onload = function(){
-    var ipUrl = "https://ipinfo.io/json?token=26844cb4019309"
-    var appId = "appid=f5e6bdcf1a910c89d1e7a7b079f45e02";
-    var location = document.getElementById("location");
+//var weatherAPIKey = "f5e6bdcf1a910c89d1e7a7b079f45e02";
+//var weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
 
-    httpReqIpAsync(ipUrl);	
-    
-
-    //ip info.io api request for location
-    function httpReqIpAsync(url, callback) {
-		var httpReqIp = new XMLHttpRequest();
-		httpReqIp.open("GET", url, true)
-		httpReqIp.onreadystatechange = function() {
-            if(httpReqIp.readyState == 4 && httpReqIp.status == 200) {
-				var jsonIp = JSON.parse(httpReqIp.responseText)
-                console.log(jsonIp);
-                
-            }
-        }
-        
-        httpReqIp.send();
-    }
+const app = {
+    init: () => {
+        app.getLocation;
+        console.log(navigator.geolocation.getCurrentPosition(app.ftw, app.wtf));
+    },
+    fetchWeather: (ev) => {
+        //use the values from latitude and longitude to fetch the weather
+        let lat = app.ftw.lat;
+        let lon = app.ftw.lon;
+        let key = 'db633226c495019f85b5f155d8f51869';
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+    //fetch the weather
+    fetch(url)
+    .then((resp) => {
+        //if not ok then throw an error
+      if (!resp.ok) throw new Error(resp.statusText);
+      //return the JSON file to extract information
+      return resp.json();
+    })
+    //Pass data to the show weather function
+    .then((data) => {
+        app.showWeather(data);
+      })
+      //Attempt to catch any errors
+      .catch(console.err);
+  },
+  getLocation: (ev) => {
+    let opts = {
+      enableHighAccuracy: true,
+      timeout: 1000 * 10, //10 seconds
+      maximumAge: 1000 * 60 * 5, //5 minutes
+    };
+    navigator.geolocation.getCurrentPosition(app.ftw, app.wtf, opts);
+  },
+  ftw: (position) => {
+    //got position
+    let lat = position.coords.latitude.toFixed(2);
+    let lon = position.coords.longitude.toFixed(2);
+  },
+  wtf: (err) => {
+    //geolocation failed
+    console.error(err);
+  },
+  showWeather: (resp) => {
+    console.log(resp);
+  }
 }
+
+app.init();
+
