274666b42cecc6a637b616a8a9db14bbe94a9c42
murtovski <murta1991@gmail.com>
2023-03-06 18:58:08 +0000

    finished styling and got the weather api working

---
+++ script.js
@@ -0,0 +1,6 @@
+let API = "http://api.weatherapi.com/v1/current.json?key=5def9735af5f4d35802153342230603 &q=Dublin&aqi=no";
+
+
+document.onload = function(){
+    console.log(API);
+}
--- weather.js
+++ weather.js
@@ -1,53 +1,42 @@
-//var weatherAPIKey = "f5e6bdcf1a910c89d1e7a7b079f45e02";
-//var weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+//let API = "http://api.weatherapi.com/v1/current.json?key=5def9735af5f4d35802153342230603 &q=Dublin&aqi=no";
 
-const app = {
-    init: () => {
-        app.getLocation;
-        console.log(navigator.geolocation.getCurrentPosition(app.ftw, app.wtf));
-    },
-    fetchWeather: (ev) => {
-        //use the values from latitude and longitude to fetch the weather
-        let lat = app.ftw.lat;
-        let lon = app.ftw.lon;
-        let key = 'db633226c495019f85b5f155d8f51869';
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
-    //fetch the weather
-    fetch(url)
-    .then((resp) => {
-        //if not ok then throw an error
-      if (!resp.ok) throw new Error(resp.statusText);
-      //return the JSON file to extract information
-      return resp.json();
+
+const apiKey = "5def9735af5f4d35802153342230603"; // replace with your WeatherAPI API key
+//const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat}, ${lon}`; // replace "London" with the location you want to get the weather for
+
+// Get user's current location
+navigator.geolocation.getCurrentPosition(position => {
+  const latitude = position.coords.latitude.toFixed(2);
+  const longitude = position.coords.longitude.toFixed(2);
+
+  // Call weather API with latitude and longitude
+  fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`)
+    .then(response => response.json())
+    .then(data => {
+      // Access weather data and do something with it
+      console.log(data);
+      setData(data);
     })
-    //Pass data to the show weather function
-    .then((data) => {
-        app.showWeather(data);
-      })
-      //Attempt to catch any errors
-      .catch(console.err);
-  },
-  getLocation: (ev) => {
-    let opts = {
-      enableHighAccuracy: true,
-      timeout: 1000 * 10, //10 seconds
-      maximumAge: 1000 * 60 * 5, //5 minutes
-    };
-    navigator.geolocation.getCurrentPosition(app.ftw, app.wtf, opts);
-  },
-  ftw: (position) => {
-    //got position
-    let lat = position.coords.latitude.toFixed(2);
-    let lon = position.coords.longitude.toFixed(2);
-  },
-  wtf: (err) => {
-    //geolocation failed
-    console.error(err);
-  },
-  showWeather: (resp) => {
-    console.log(resp);
-  }
+    .catch(error => console.error(error));
+}, error => console.error(error));
+
+function setData(data){
+  getID("location").innerHTML = `${data.location.name}, ${data.location.country}`;
+  getID("desc").innerHTML = `${data.current.condition.text}`;
+  const icon = getID("icon-desc");
+  icon.src = "https:" + data.current.condition.icon;
+  getID("humidity-data").innerHTML = `${data.current.humidity}%`
+  getID("wind-data").innerHTML = `${data.current.gust_mph} mph`
+  getID("direction-data").innerHTML = `${data.current.wind_degree}&deg`
+  temp = Math.round(data.current.feelslike_c);
+  getID("temperature-txt").innerHTML = `${temp}&degC`
 }
 
-app.init();
-
+function getID(id) {
+    return document.getElementById(id);
+  }
+  
+function getClass(className) {
+    return document.getElementsByClassName(className);
+  }
+  
